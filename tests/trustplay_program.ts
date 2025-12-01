ef443058939010da2d70327d194e70d97f59053a
deepesh-sr <rathoredeepeshsingh@gmail.com>
2025-12-02 01:58:40 +0530

    initial commit :0

---
+++ tests/trustplay_program.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { TrustplayProgram } from "../target/types/trustplay_program";
+
+describe("trustplay_program", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.trustplayProgram as Program<TrustplayProgram>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
