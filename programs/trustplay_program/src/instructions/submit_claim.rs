d9c6b9bdd83bcd5d153d6ee2938181794fa3f81d
deepesh-sr <220714100078@cutm.ac.in>
2025-12-09 11:22:55 +0530

    add ix claim

---
+++ programs/trustplay_program/src/instructions/submit_claim.rs
@@ -0,0 +1,38 @@
+use anchor_lang::prelude::*;
+
+use crate::Claim;
+
+#[derive(Accounts)]
+#[instruction(claim_id: String)]
+pub struct SubmitClaim<'info> {
+    #[account(
+        mut, 
+        seeds = [b"room", room.organizer.as_ref(), room.room_id.as_bytes()], 
+        bump = room.bump
+    )]
+    pub room: Account<'info, Room>,
+
+    #[account(init, payer = claimant, space = 8 + Claim::INIT_SPACE , seeds = [b"claim", room.key().as_ref(), claimant.key.as_ref(), claim_id.as_bytes()], bump)]
+    pub claim: Account<'info, Claim>,
+
+    #[account(mut)]
+    pub claimant: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl <'info> SubmitClaim<'info>{
+
+    pub fn submit_claim(&mut self, claim_id: String, proof_hash: String)-> Result<()>{
+         let claim = &mut self.claim;
+        claim.claimant = claimant.key();
+        claim.claim_id = claim_id;
+        claim.proof_hash = proof_hash;
+        claim.votes_for = 0;
+        claim.votes_against = 0;
+        claim.resolved = false;
+        claim.created_at = Clock::get()?.unix_timestamp;
+        claim.bump = self.claim.bump ;
+        Ok(())
+    }
+}
